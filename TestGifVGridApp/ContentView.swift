7a6d10d5e35e28cb752192e4faa8d8b54aad570f
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2023-01-29 03:46:52 -0500

    initial commit to show issue

--- TestGifVGridApp/ContentView.swift
+++ TestGifVGridApp/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ContentView: View {
+    var items: [GridItem] = Array(repeating: .init(.adaptive(minimum: 400, maximum: 1000)), count: 4)
+    @State var gifs: [GifObjectShell] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView{
+            LazyVGrid(columns: items) {
+                ForEach(gifs) { gif in
+                    AnimatedImage(url: URL(string: gif.data.images.fixed_height_downsampled.url))
+                        .resizable()
+                        .scaledToFit()
+                        .contextMenu {
+                            Button("Copy GIF URL") {
+                                NSPasteboard.general.setString(gif.data.images.fixed_height_downsampled.url, forType: .string)
+                            }
+                        }
+                }
+            }
         }
-        .padding()
+        .task {
+            gifs = try! await pullImages()
+        }
     }
 }
 
--- TestGifVGridApp/TestGifVGridAppApp.swift
+++ TestGifVGridApp/TestGifVGridAppApp.swift
@@ -15,3 +15,36 @@ struct TestGifVGridAppApp: App {
         }
     }
 }
+
+struct GifObject: Codable {
+    let images: Images
+}
+
+struct GifObjectShell: Codable, Identifiable {
+    var id = UUID()
+    
+    let data: GifObject
+    
+    private enum CodingKeys : String, CodingKey {
+        case data
+    }
+}
+
+struct Object: Codable {
+    let url: String
+}
+
+struct Images: Codable {
+    let fixed_height_downsampled: Object
+}
+
+func pullImages() async throws -> [GifObjectShell] {
+    var gifObjects: [GifObjectShell] = []
+    for i in 0...100 {
+        let request = URLRequest(url: URL(string: "https://api.giphy.com/v1/gifs/random?api_key=oCBbE8KLSchvtU06IIFCGJiGGsQUXhBJ&tag=&rating=g")!)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        let parsedData = try JSONDecoder().decode(GifObjectShell.self, from: data)
+        gifObjects.append(parsedData)
+    }
+    return gifObjects
+}
