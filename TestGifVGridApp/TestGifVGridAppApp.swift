fa9f5691f5f132c5df05096712d8e8124feb50be
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2023-01-29 03:19:22 -0500

    Initial Commit

---
+++ TestGifVGridApp/TestGifVGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGifVGridAppApp.swift
+//  TestGifVGridApp
+//
+//  Created by Andy Lin on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TestGifVGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
